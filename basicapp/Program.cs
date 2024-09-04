fdacfaf4ff04157b5b9f41b71e790fda3031d8e3
Your Name <you@example.com>
2024-09-04 08:04:55 +0545

    learn about datatypes

--- basicapp/Program.cs
+++ basicapp/Program.cs
@@ -1,7 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//oop:object oriented programming with c#
+//static vs dynamic
+//00p vs functional vs procedural
+//imperative vs declarative
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+//Datatype
+//numbers
+//integers
+byte ageofperson=45;
+
+
+byte personAge=56;
+short students=2342;
+int countrypopulation=23456;
+long weightofearth=675432409;
+//floating point number:precision(accuracy)
+float elephantWeight=3457.567755f;
+double freezerPrice=342345.234564321;
+decimal moleculeWeight=43.34234523344444m;
+//string 
+string collegename="Nast";
+char gender='M';
+
+//Boolean
+bool isAbsentToday = true;
+
+//custom
+DateofTime dob=DateTime.Now;
+
+var fulname=89; //type inference 
+
+int? marks;//All type
+marks=45;
+marks=null;
+
+
+console.writeline(personAge);
